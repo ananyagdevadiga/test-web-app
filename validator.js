6f259cce39070d19f0c905ed6b985f49b971a101
Ananya G Devadiga <155678809+ananyagdevadiga@users.noreply.github.com>
2026-03-14 12:38:33 +0530

    ..

--- validator.js
+++
@@ -1,8 +0,0 @@
-function validateLoginForm(username, password) {
-  if (!username || !password) {
-    return { isValid: false, message: 'Username and password cannot be empty.' };
-  }
-  return { isValid: true };
-}
-
-export { validateLoginForm };
